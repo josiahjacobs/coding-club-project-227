9c6d2f5ac0409ad1902734053d1a6aa34615d266
josiahjacobs <76837927+josiahjacobs@users.noreply.github.com>
2023-02-27 10:42:03 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "!RJB-%N{6NS=Ex2HB+4-":
+            case "badguy2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . f e 1 e e e 1 f . . . . . 
+. . f e e e 1 e 1 e e f . . . . 
+. . f e e e e e e e e e f . . . 
+. f e e e 1 e e e 1 e e f . . . 
+. f e e e 1 e e e 1 e e e f . . 
+. f e e e e e e e e e e e e f . 
+. f e e e e 1 1 1 e e e e e f . 
+. . f e e 1 1 e 1 1 e e e e f . 
+. . f e e 1 e e e 1 1 e e e f . 
+. . . f e e e e e e e e e e f . 
+. . . . f f e e e e e e e f . . 
+. . . . . . f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
